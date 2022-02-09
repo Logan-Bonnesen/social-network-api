63ccd7c2bff44e7430a075c5016c65cf3c2a3448
Logan-Bonnesen <91345048+Logan-Bonnesen@users.noreply.github.com>
2022-02-08 19:14:57 -0700

    updating server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const connection = require('./config/connection');
+const routes = require('./routes');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(routes);
+
+connection.once('open', () => {
+    app.listen(PORT, () => 
+console.log(`===== Now listening on port ${PORT} =====`));
+});
+
+
